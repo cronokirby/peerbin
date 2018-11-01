c53f0ca320a16f23a293fc3102fea841a1327ee9
Lúcás Meier <cronokirby@gmail.com>
2018-11-01 18:31:55 +0100

    Add basic language handling

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,22 +1,30 @@
 module Main exposing (main)
 
 import Browser
-import Html as H exposing (Html)
+import Html as H exposing (Html, form)
 import Html.Attributes as H
 import Html.Events as H
 
 
 type alias Model =
-    { text : String }
+    { text : String
+    , languageInput : String
+    , language : String
+    }
 
 
 initialModel : Model
 initialModel =
-    { text = "" }
+    { text = ""
+    , languageInput = "plaintext"
+    , language = "plaintext"
+    }
 
 
 type Msg
     = InputText String
+    | InputLanguage String
+    | SetLanguage
 
 
 update : Msg -> Model -> Model
@@ -25,27 +33,59 @@ update msg model =
         InputText txt ->
             { model | text = txt }
 
+        InputLanguage lang ->
+            { model | languageInput = lang }
+
+        SetLanguage ->
+            { model | language = model.languageInput }
+
 
 view : Model -> Html Msg
 view model =
     H.div [ H.class "container" ]
-        [ header
+        [ header model
         , textArea model
         ]
 
 
-header : Html Msg
-header =
+header : Model -> Html Msg
+header model =
     H.div [ H.class "header" ]
-        [ H.text "Peer Bin"
+        [ H.div [ H.class "header-title" ]
+            [ H.text model.language
+            ]
+        , H.div [ H.class "header-lang" ]
+            [ H.input
+                [ H.onInput InputLanguage
+                , H.value model.languageInput
+                , H.onSubmit SetLanguage
+                ]
+                []
+            , H.button [ H.onClick SetLanguage ]
+                [ H.text "set"
+                ]
+            ]
         ]
 
+languageForm : Model -> Html Msg
+languageForm model =
+    H.div [ H.class "header-lang" ]
+        [ form []
+            [ H.input 
+                [ H.type_ "text" 
+                , H.value model.languageInput
+                , H.onInput InputLanguage
+                ]
+                []
+            ]
+        ]
 
 textArea : Model -> Html Msg
 textArea model =
     H.div [ H.class "code-area" ]
         [ H.textarea
             [ H.placeholder "Enter code here"
+            , H.spellcheck False
             , H.onInput InputText
             ]
             []
